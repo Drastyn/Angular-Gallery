4665e06f1bbece64fe3387dbd2d3c5f5f0dc43a6
Drastyn <sheizck@gmail.com>
2021-06-09 23:05:20 -0400

    Added font awesome icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 //COMPONENTS
 import { LoginComponent } from './components/auth/login.component';
@@ -9,6 +12,8 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ImagesComponent } from './components/dashboard/children/images/images.component';
 import { ImageNewComponent } from './components/dashboard/children/images/image/image-new.component';
 import { ImageComponent } from './components/dashboard/children/images/image/image.component';
+import { CreditsComponent } from './components/dashboard/templates/credits.component';
+import { FooterComponent } from './components/dashboard/templates/footer.component';
 //SERVICES
 import { LoginService } from './services/login.service';
 import { ImageService } from './services/images.service';
@@ -23,9 +28,21 @@ import { watchDog } from './services/login-guardian.services';
     ImagesComponent,
     ImageNewComponent,
     ImageComponent,
+    CreditsComponent,
+    FooterComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FontAwesomeModule,
+  ],
   providers: [LoginService, ImageService, watchDog],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, fab);
+  }
+}
---
+++ src/app/components/dashboard/templates/credits.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-credits',
+  templateUrl: './credits.component.html',
+})
+export class CreditsComponent implements OnInit {
+  ngOnInit(): void {}
+}
